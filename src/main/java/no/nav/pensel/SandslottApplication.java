ffea8eab3d0d142bf812888cbd7ea5afe9f993a6
eogjostol <36151316+eogjostol@users.noreply.github.com>
2022-11-23 22:34:02 +0100

    Create SandslottApplication.java

---
+++ src/main/java/no/nav/pensel/SandslottApplication.java
@@ -0,0 +1,12 @@
+package no.nav.pensel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandslottApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandslottApplication.class, args);
+    }
+}
